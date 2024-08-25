2fb8cc4ef18268fe050ba93368204d882440ee34
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-08-25 04:40:51 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const makeChange = (c) => {
-  // your name here
-	c=parseInt(c,10);
-	let change={
-		q:0;
-		n:0;
-		d:0;
-		p:0;
-	}
+  c = parseInt(c, 10);
+  let change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  }
 
+  change.q = Math.floor(c / 25);
+  c = c % 25;
+  change.d = Math.floor(c / 10);
+  c = c % 10;
+  change.n = Math.floor(c / 5);
+  c = c % 5;
+  change.p = c;
 
-change.q=Math.floor(c/25);
-c=c%25;
-change.n=Math.floor(c/5);
-c=c%5;
-change.d=Math.florr(c/10);
-c=c%10;
-change.p=c;
-return change;
+  return change;
 };
+
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
